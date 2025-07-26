276efbc774b9e4a1f604c5b039050eb068a11a66
Clevison <cleversonmaiden031@gmail.com>
2025-07-26 00:58:43 -0500

    finalizado

---
+++ script.js
@@ -0,0 +1,26 @@
+function clicar() {
+      let input = document.getElementById('txtab')
+      let n = Number(input.value)
+
+      let soma = document.getElementById('soma')
+      let subtracao = document.getElementById('subtracao')
+      let multiplicacao = document.getElementById('multiplicacao')
+      let divisao = document.getElementById('divisao')
+
+      if (input.value === '') {
+        window.alert('digite um número!')
+        return
+      }
+
+      soma.innerHTML = ''
+      subtracao.innerHTML = ''
+      multiplicacao.innerHTML = ''
+      divisao.innerHTML = ''
+
+      for (let c = 1; c <= 20; c++) {
+        soma.innerHTML += `${n} + ${c} = ${n + c}<br>`
+        subtracao.innerHTML += `${n} - ${c} = ${n - c}<br>`
+        multiplicacao.innerHTML += `${n} * ${c} = ${n * c}<br>`
+        divisao.innerHTML += `${n} / ${c} = ${(n / c).toFixed(2)}<br>`
+      }
+    }
